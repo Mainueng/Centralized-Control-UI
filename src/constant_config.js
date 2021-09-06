ed5f1c71899f9dfd6e4545685ab8975db09b860e
p.chamsing@gmail.com <p.chamsing@gmail.com>
2021-09-06 13:09:38 +0700

    edit production config

--- src/constant_config.js
+++ src/constant_config.js
@@ -1,15 +1,15 @@
 module.exports = {
   /* Production */
-  // SRV_SERV_API: "http://10.126.58.60:3002",
-  // SRV_CRON_API: "http://10.126.58.60:3030",
-  // SERV_WWS_API: "http://10.126.58.60:8000",
-  // SERV_LOCAL_API: "http://10.126.58.60:5000",
+  SRV_SERV_API: "http://10.126.58.60:3002",
+  SRV_CRON_API: "http://10.126.58.60:3030",
+  SERV_WWS_API: "http://10.126.58.60:8000",
+  SERV_LOCAL_API: "http://10.126.58.60:5000",
 
   /* DEV */
-  SRV_SERV_API: "http://192.168.11.190:3002",
-  SRV_CRON_API: "http://192.168.11.190:3030",
-  SERV_WWS_API: "http://192.168.11.190:8000",
+  // SRV_SERV_API: "http://192.168.11.190:3002",
+  // SRV_CRON_API: "http://192.168.11.190:3030",
+  // SERV_WWS_API: "http://192.168.11.190:8000",
   // SERV_LOCAL_API: "http://192.168.11.190:5000",
   // SERV_WWS_API: "http://localhost:5000",
-  SERV_LOCAL_API: "http://localhost:5000",
+  // SERV_LOCAL_API: "http://localhost:5000",
 };
